efa351befb3063105275e673ee1f26ede8ba9ab9
faisal <faisalfnadaf@gmail.com>
2024-01-25 20:51:15 +0530

    code

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-let email;
-let password;
 
-function registerUser() {
-     email = document.getElementById('email').value;
-     password = document.getElementById('password').value;
+var users = [];
 
-    var user = {
-        email: email,
-        password: password,
-    };
+function addUser() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-    console.log(user);
+  if (email && password) {
+    if (!userExists(email)) {
+      users.push({ email, password });
+      alert("User registered successfully!");
+     
+    } else {
+      alert("email already registered. Please use a different email.");
+    }
+  } else {
+    alert("Both email and password are required.");
+  }
+}
+
+function userExists(email) {
+  return users.some(user => user.email === email);
+}
+
+function loginUser() {
+  var email = document.getElementById("loginemail").value;
+  var password = document.getElementById("loginpassword").value;
+
+  var user = users.find(user => user.email === email && user.password === password);
+
+  if (user) {
+    alert("Login successful!");
+  } else {
+    alert("Invalid email or password. Please try again.");
+  }
 }
